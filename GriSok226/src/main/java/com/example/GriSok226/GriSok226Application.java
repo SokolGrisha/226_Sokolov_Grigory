159cb431f015862024644589137f65eb00ebc822
Grigory Sokolov <grsha9@gmail.com>
2024-03-20 21:50:07 +0300

    First lab start

---
+++ GriSok226/src/main/java/com/example/GriSok226/GriSok226Application.java
@@ -0,0 +1,13 @@
+package com.example.GriSok226;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriSok226Application {
+	public static void main(String[] args) {
+		System.out.println("First Project!");
+		SpringApplication.run(GriSok226Application.class, args);
+	}
+
+}
---
+++ GriSok226/src/test/java/com/example/GriSok226/GriSok226ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GriSok226;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GriSok226ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
