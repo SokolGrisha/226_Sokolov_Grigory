22f820877826baea32d71aca10a4049daea6762f
Grigory Sokolov <grsha9@gmail.com>
2024-03-21 09:38:12 +0300

    First lab overtake

--- GriSok226/src/test/java/com/example/GriSok226/GriSok226ApplicationTests.java
+++ GriSok226/src/test/java/com/example/GriSok226/GriSok226ApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.GriSok226;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GriSok226ApplicationTests {
 
-	@Test
+	@BeforeAll
 	void contextLoads() {
+		System.out.println("@Before");
 	}
 
 }
