e3a5272c87c3dfa69389c0fa6158337687e703ea
Grigory Sokolov <grsha9@gmail.com>
2024-03-21 11:02:38 +0300

    2h Lab1 created | overtake

---
+++ Lab1_GS/src/main/java/com/example/Lab1_GS/Lab1GsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab1_GS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1GsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1GsApplication.class, args);
+	}
+
+}
